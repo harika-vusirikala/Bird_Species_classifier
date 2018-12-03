f1f5beb72ef5a9945c0171ab1ebb2e132563a7e2
Harika <31295103+harika-vusirikala@users.noreply.github.com>
2018-12-03 14:43:41 -0600

    Update Train_SVM.py

--- Train_SVM.py
+++ Train_SVM.py
@@ -5,7 +5,7 @@ from keras.preprocessing import image
 from keras.models import Sequential
 from sklearn import svm
 from keras.layers import *
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,classification_report
 
 x_train = np.load("data/train_data_BW.npy")
 x_test = np.load("data/test_data_BW.npy")
@@ -15,9 +15,6 @@ y_test = np.load("data/test_labels_BW.npy")
 num_classes = 200
 #print(y_train)
 
-seed = 5
-np.random.seed(seed)
-
 s=np.arange(x_train.shape[0])
 np.random.shuffle(s)
 x_train = x_train[s]
@@ -26,11 +23,11 @@ y_train = y_train[s]
 #x_train.reshape(len(x_train),-1)
 print(x_train.shape)
 
-svm_classifier = svm.SVC(C=100.0,gamma = 'auto',degree=4)
+svm_classifier = svm.SVC(C=100.0,gamma = 'auto',degree=5)
 training_history = svm_classifier.fit(x_train,y_train)
 
 y_pred = svm_classifier.predict(x_test)
-accuracy = accuracy_score(y_test,y_pred)
+accuracy = classification_report(y_test,y_pred)
 print(accuracy)
 
 
