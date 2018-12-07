03610a4f5ee130d34d5343530c3d8411bfc9f013
Harika <31295103+harika-vusirikala@users.noreply.github.com>
2018-12-06 18:30:42 -0600

    Update bird_classifier_test.py

--- bird_classifier_test.py
+++ bird_classifier_test.py
@@ -1,3 +1,11 @@
+'''Name        : bird_classifier_test.py
+   Date        : 12/19/18
+   Author      : Harika Vusirikala
+   Description : This program tests the trained model with test images
+   Dependencies : data/test_data.npy,
+                  data/test_labels.npy,
+                Model/Bird_classifier_adam_v8.h5(Model to be tested)
+   '''
 import numpy as np
 import keras
 from keras.preprocessing import image
@@ -5,21 +13,22 @@ from keras.models import load_model
 from keras.layers import *
 import matplotlib.pyplot as plt
 
+'''Loading test data and test labels from local disk'''
 x_test = np.load("data/test_data.npy")
 y_test = np.load("data/test_labels.npy")
 
-'''s=np.arange(x_test.shape[0])
-np.random.shuffle(s)
-x_test = x_test[s]
-y_test = y_test[s]
-print(y_test.shape)'''
+'''Number of classes is set to 200'''
+num_classes = 200
+#y_test = y_test.astype('float32')-1
 
-num_classes = 201
-
+'''Encoding the test samples with one-hot encoding'''
 y_test = keras.utils.to_categorical(y_test,num_classes)
 
-input_shape = (100,100,3)
-model = load_model("Bird_classifier_model3.h5")
+'''Loading the model from the disk'''
+model = load_model("Model/Bird_classifier_adam_v8.h5")
+model.summary()
 
+'''Testing the model with test data and their labels'''
 score = model.evaluate(x_test,y_test,verbose=2)
-print('Test accuracy: ',score[1])
+'''Printing the test accuracy'''
+print('Test accuracy:\n ',score)
